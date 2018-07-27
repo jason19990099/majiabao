615164df4d70ef92d6c1220eeda05c9d6097405e
DESKTOP-VFHMO2M\o <jason19990099@gmail.com>
2018-07-27 16:03:52 +0800

    123

--- app/src/main/java/com/gzsll/hupu/Base.java
+++ app/src/main/java/com/gzsll/hupu/Base.java
@@ -2,7 +2,7 @@ package com.gzsll.hupu;
 
 public class Base {
     public static  String majiabao_url = "https://hk1.android.jrapp.me/switch/";
-    public static  String appid = "a1007";
+    public static  String appid = "a1005";
     public static  String share_url = "https://www.appkings.me/xpj.html";
     //注意要修改配置文件的推送設置
 }
